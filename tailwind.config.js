1e5c582e84919a8c7358f9b09d0708491f5e9722
oaktec <54672803+oaktec@users.noreply.github.com>
2023-01-26 14:38:02 +0000

    Improve aesthetic

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      minHeight: {
+        "50vmin": "50vmin",
+      },
+      minWidth: {
+        "50vmin": "50vmin",
+      },
+    },
   },
   plugins: [],
 };
